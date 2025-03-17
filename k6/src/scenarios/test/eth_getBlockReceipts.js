c55585b7bf04e6ea8754f8379ef4a8cd51fea042
Konstantina <konstantina.blazhukova@limechain.tech>
2025-03-17 15:56:29 +0200

    adds k6 test
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

---
+++ k6/src/scenarios/test/eth_getBlockReceipts.js
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import http from 'k6/http';
+
+import { TestScenarioBuilder } from '../../lib/common.js';
+import { isNonErrorResponse, httpParams, getPayLoad } from './common.js';
+
+const url = __ENV.RELAY_BASE_URL;
+
+const methodName = 'eth_getBlockReceipts';
+const { options, run } = new TestScenarioBuilder()
+  .name(methodName)
+  .request((testParameters) => {
+    return http.post(
+      testParameters.RELAY_BASE_URL,
+      getPayLoad(methodName, [testParameters.DEFAULT_BLOCK_HASH]),
+      httpParams,
+    );
+  })
+  .check(methodName, (r) => isNonErrorResponse(r))
+  .testDuration('3s')
+  .build();
+
+export { options, run };
