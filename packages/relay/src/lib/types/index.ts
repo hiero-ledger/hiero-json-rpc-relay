013d4423d41ad7252ff21cd3cd1145d1ed8d70e8
Logan Nguyen <logan.nguyen@swirldslabs.com>
2025-03-19 16:29:05 -0500

    chore: cleaned up types/index.ts
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- packages/relay/src/lib/types/index.ts
+++ packages/relay/src/lib/types/index.ts
@@ -1,52 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { IFeeHistory } from './IFeeHistory';
-import { ITransactionRecordMetric } from './metrics';
-import { ITransactionReceipt } from './ITransactionReceipt';
-import { ITracerConfigWrapper } from './ITracerConfigWrapper';
-import { IExecuteQueryEventPayload, IExecuteTransactionEventPayload } from './events';
-import { ICallTracerConfig, IOpcodeLoggerConfig, ITracerConfig } from './ITracerConfig';
-import {
-  IAssessedCustomFee,
-  IContractCallRequest,
-  IContractCallResponse,
-  IContractLogsResultsParams,
-  IContractResultsParams,
-  ILimitOrderParams,
-  IMirrorNodeTransactionRecord,
-  INftTransfer,
-  IStakingRewardTransfer,
-  ITokenTransfer,
-  ITransfer,
-  MirrorNodeTransactionRecord,
-} from './mirrorNode';
-import { IRequestDetails, RequestDetails } from './RequestDetails';
-import { IGetLogsParams, INewFilterParams } from './requestParams';
-
-export {
-  ITransfer,
-  IFeeHistory,
-  INftTransfer,
-  ITracerConfig,
-  ITokenTransfer,
-  IGetLogsParams,
-  INewFilterParams,
-  ILimitOrderParams,
-  ICallTracerConfig,
-  IAssessedCustomFee,
-  IOpcodeLoggerConfig,
-  ITransactionReceipt,
-  IContractCallRequest,
-  ITracerConfigWrapper,
-  IContractCallResponse,
-  IStakingRewardTransfer,
-  IContractResultsParams,
-  ITransactionRecordMetric,
-  IExecuteQueryEventPayload,
-  IContractLogsResultsParams,
-  MirrorNodeTransactionRecord,
-  IMirrorNodeTransactionRecord,
-  IExecuteTransactionEventPayload,
-  IRequestDetails,
-  RequestDetails,
-};
+export * from './events';
+export * from './IFeeHistory';
+export * from './ITracerConfig';
+export * from './ITracerConfigWrapper';
+export * from './ITransactionReceipt';
+export * from './metrics';
+export * from './mirrorNode';
+export * from './RequestDetails';
+export * from './requestParams';
+export * from './spendingPlanConfig';
