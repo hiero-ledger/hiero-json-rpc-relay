11f2d791e1a856ab5d956550ff5bafed33372a50
nikolay <n.atanasow94@gmail.com>
2025-03-24 11:25:48 +0200

    chore: fix test
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- packages/relay/tests/lib/hedera.spec.ts
+++ packages/relay/tests/lib/hedera.spec.ts
@@ -4,17 +4,17 @@ import { expect } from 'chai';
 import pino from 'pino';
 import { Registry } from 'prom-client';
 
-import { RelayImpl } from '../../src/lib/relay';
+import { Relay } from '../../src/lib/relay';
 import { RequestDetails } from '../../src/lib/types';
 
 const logger = pino({ level: 'silent' });
-let Relay;
+let relay;
 
 const requestDetails = new RequestDetails({ requestId: 'eth_estimateGasTest', ipAddress: '0.0.0.0' });
 describe('Hedera', async function () {
   it('should execute admin.config', async () => {
-    Relay = new RelayImpl(logger, new Registry());
-    const res = await Relay.admin().config(requestDetails);
+    relay = new Relay(logger, new Registry());
+    const res = await relay.admin().config(requestDetails);
     expect(res).to.haveOwnProperty('relay');
     expect(res).to.haveOwnProperty('upstreamDependencies');
 
