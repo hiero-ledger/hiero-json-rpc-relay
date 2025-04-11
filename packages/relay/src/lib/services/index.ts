a720b412ac8837d833745221a0fdad12f3f60e9e
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2025-04-11 12:38:52 +0300

    simplify services index file
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- packages/relay/src/lib/services/index.ts
+++ packages/relay/src/lib/services/index.ts
@@ -1,24 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
-import { BlockService } from './blockService/BlockService';
-import { IBlockService } from './blockService/IBlockService';
-import { ContractService } from './contractService/ContractService';
-import { IContractService } from './contractService/IContractService';
-import { CommonService } from './ethService/ethCommonService/CommonService';
-import { ICommonService } from './ethService/ethCommonService/ICommonService';
-import { FilterService } from './ethService/ethFilterService';
-import { IFilterService } from './ethService/ethFilterService/IFilterService';
-import MetricService from './metricService/metricService';
-import { TransactionService } from './transactionService/TransactionService';
-
-export {
-  BlockService,
-  IBlockService,
-  ICommonService,
-  CommonService,
-  IFilterService,
-  FilterService,
-  IContractService,
-  ContractService,
-  TransactionService,
-  MetricService,
-};
+export * from './blockService/BlockService';
+export * from './blockService/IBlockService';
+export * from './contractService/ContractService';
+export * from './contractService/IContractService';
+export * from './ethService/ethCommonService/CommonService';
+export * from './ethService/ethCommonService/ICommonService';
+export * from './ethService/ethFilterService';
+export * from './ethService/ethFilterService/IFilterService';
+export * from './transactionService/TransactionService';
+export * from './metricService/metricService';
