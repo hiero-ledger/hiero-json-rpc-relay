fabe37350a54da537a7853ec7ab1b72025194c5b
Simeon Nakov <simeon.nakov@limechain.tech>
2025-04-30 12:43:53 +0300

    remove comment
    Signed-off-by: Simeon Nakov <simeon.nakov@limechain.tech>

--- packages/relay/src/lib/eth.ts
+++ packages/relay/src/lib/eth.ts
@@ -2931,7 +2931,6 @@ export class EthImpl implements Eth {
     }
 
     transactionArray = this.populateSyntheticTransactions(showDetails, logs, transactionArray, requestDetails);
-    // transactionArray = _.uniqWith(transactionArray, _.isEqual);
     const formattedReceipts: IReceiptRootHash[] = ReceiptsRootUtils.buildReceiptRootHashes(
       transactionArray.map((tx) => (showDetails ? tx.hash : tx)),
       contractResults,
