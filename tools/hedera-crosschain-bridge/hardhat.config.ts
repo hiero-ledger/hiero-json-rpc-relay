4eda07e7c938d6190bff311eb2dd048faa1b5041
Logan Nguyen <logan.nguyen@swirldslabs.com>
2025-05-28 10:09:31 -0500

    feat: hardhat init
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

---
+++ tools/hedera-crosschain-bridge/hardhat.config.ts
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+import '@nomicfoundation/hardhat-toolbox';
+
+import { HardhatUserConfig } from 'hardhat/config';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.28',
+};
+
+export default config;
