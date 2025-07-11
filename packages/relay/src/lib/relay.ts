2a4459baa1e1697b4aeb1cc0a00c266ad6a2bfbf
Luis Mastrangelo <luis@swirldslabs.com>
2025-07-11 20:53:16 +0200

    Log operator balance
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- packages/relay/src/lib/relay.ts
+++ packages/relay/src/lib/relay.ts
@@ -331,12 +331,14 @@ export class Relay {
   }
 
   async ensureOperatorHasBalance() {
-    if (!ConfigService.get('READ_ONLY')) {
-      const operator = this.clientMain.operatorAccountId!.toString();
-      const balance = await this.ethImpl.getBalance(operator, 'latest', {} as RequestDetails);
-      if (BigInt(balance) === BigInt(0)) {
-        throw new Error(`Operator account \`${operator}\` has no balance`);
-      }
+    if (ConfigService.get('READ_ONLY')) return;
+
+    const operator = this.clientMain.operatorAccountId!.toString();
+    const balance = BigInt(await this.ethImpl.getBalance(operator, 'latest', {} as RequestDetails));
+    if (balance === BigInt(0)) {
+      throw new Error(`Operator account \`${operator}\` has no balance`);
+    } else {
+      this.logger.info(`Operator account \`${operator}\` has balance: ${balance}`);
     }
   }
 }
