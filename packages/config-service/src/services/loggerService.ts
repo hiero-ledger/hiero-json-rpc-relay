78881be42c97f0afac8197699d2e62213fbdc776
nikolay <n.atanasow94@gmail.com>
2025-07-23 13:53:41 +0300

    chore: simplify config object
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- packages/config-service/src/services/loggerService.ts
+++ packages/config-service/src/services/loggerService.ts
@@ -3,19 +3,7 @@
 import { GlobalConfig } from './globalConfig';
 
 export class LoggerService {
-  public static readonly SENSITIVE_FIELDS = [
-    GlobalConfig.ENTRIES.OPERATOR_KEY_MAIN.envName,
-    GlobalConfig.ENTRIES.GITHUB_TOKEN.envName,
-    GlobalConfig.ENTRIES.GH_ACCESS_TOKEN.envName,
-  ];
-
-  public static readonly KNOWN_SECRET_PREFIXES = [
-    'ghp', // GitHub personal access tokens
-    'gho', // OAuth access tokens
-    'ghu', // GitHub user-to-server tokens
-    'ghs', // GitHub server-to-server tokens
-    'ghr', // refresh tokens
-  ];
+  public static readonly SENSITIVE_FIELDS = ['OPERATOR_KEY_MAIN', 'GITHUB_TOKEN', 'GH_ACCESS_TOKEN'];
 
   public static readonly GITHUB_SECRET_PATTERN: RegExp =
     /^(gh[pousr]_[a-zA-Z0-9]{36,251}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59})$/;
--- packages/config-service/tests/src/services/loggerService.spec.ts
+++ packages/config-service/tests/src/services/loggerService.spec.ts
@@ -3,9 +3,10 @@
 import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import crypto from 'crypto';
+
+import { GlobalConfig } from '../../../dist/services/globalConfig';
 import { ConfigService } from '../../../src/services';
 import { LoggerService } from '../../../src/services/loggerService';
-import { GlobalConfig } from '../../../dist/services/globalConfig';
 
 chai.use(chaiAsPromised);
 
@@ -15,16 +16,6 @@ describe('LoggerService tests', async function () {
       const hex = crypto.randomBytes(32).toString('hex');
       const res = LoggerService.maskUpEnv(sensitiveField, hex);
       expect(res).to.equal(`${sensitiveField} = **********`);
-    }
-  });
-
-  it('should be able to mask every value if it starts with known secret prefix', async () => {
-    const { envName } = GlobalConfig.ENTRIES.OPERATOR_KEY_MAIN;
-
-    for (const prefix of LoggerService.KNOWN_SECRET_PREFIXES) {
-      const value = prefix + '_VVurqVVh68wgxgcVjrvVVVcNcVVVVi3CRwl1';
-      const res = LoggerService.maskUpEnv(envName, value);
-      expect(res).to.equal(`${envName} = **********`);
     }
   });
 
