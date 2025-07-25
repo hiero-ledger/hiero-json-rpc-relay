18085aedb92cd278baa6a674981f860e4778a05a
Nikolay Atanasow <n.atanasow94@gmail.com>
2025-07-25 15:44:20 +0300

    feat: simplify `_CONFIG` object structure (#3979)
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- packages/config-service/src/services/loggerService.ts
+++ packages/config-service/src/services/loggerService.ts
@@ -1,21 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { GlobalConfig } from './globalConfig';
+import { ConfigKey, GlobalConfig } from './globalConfig';
 
 export class LoggerService {
-  public static readonly SENSITIVE_FIELDS = [
-    GlobalConfig.ENTRIES.OPERATOR_KEY_MAIN.envName,
-    GlobalConfig.ENTRIES.GITHUB_TOKEN.envName,
-    GlobalConfig.ENTRIES.GH_ACCESS_TOKEN.envName,
-  ];
-
-  public static readonly KNOWN_SECRET_PREFIXES = [
-    'ghp', // GitHub personal access tokens
-    'gho', // OAuth access tokens
-    'ghu', // GitHub user-to-server tokens
-    'ghs', // GitHub server-to-server tokens
-    'ghr', // refresh tokens
-  ];
+  public static readonly SENSITIVE_FIELDS: ConfigKey[] = ['OPERATOR_KEY_MAIN', 'GITHUB_TOKEN', 'GH_ACCESS_TOKEN'];
 
   public static readonly GITHUB_SECRET_PATTERN: RegExp =
     /^(gh[pousr]_[a-zA-Z0-9]{36,251}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59})$/;
@@ -27,7 +15,7 @@ export class LoggerService {
    * @param envValue
    */
   static maskUpEnv(envName: string, envValue: string | undefined): string {
-    const isSensitiveField: boolean = this.SENSITIVE_FIELDS.indexOf(envName) > -1;
+    const isSensitiveField: boolean = (this.SENSITIVE_FIELDS as string[]).indexOf(envName) > -1;
     const isKnownSecret: boolean =
       GlobalConfig.ENTRIES[envName].type === 'string' && !!this.GITHUB_SECRET_PATTERN.exec(envValue ?? '');
 
