e2b5f790c894e1ca0aebadfdf636b64e4327ab1a
Logan Nguyen <logan.nguyen@swirldslabs.com>
2025-07-28 10:24:00 -0400

    feat: increased k6 coverage for all RPC endpoints (#3977)
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

---
+++ k6/src/scenarios/test/eth_maxPriorityFeePerGas.js
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+
+
+
+import http from 'k6/http';
+
+import { TestScenarioBuilder } from '../../lib/common.js';
+import { getPayLoad,httpParams, isNonErrorResponse } from './common.js';
+
+const methodName = 'eth_maxPriorityFeePerGas';
+const { options, run } = new TestScenarioBuilder()
+  .name(methodName) // use unique scenario name among all tests
+  .request((testParameters) => http.post(testParameters.RELAY_BASE_URL, getPayLoad(methodName), httpParams))
+  .check(methodName, (r) => isNonErrorResponse(r))
+  .build();
+
+export { options, run };
---
+++ k6/src/scenarios/test/web3_sha3.js
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Apache-2.0
+
+
+
+import http from 'k6/http';
+
+import { TestScenarioBuilder } from '../../lib/common.js';
+import { getPayLoad,httpParams, isNonErrorResponse } from './common.js';
+
+const methodName = 'web3_sha3';
+const { options, run } = new TestScenarioBuilder()
+  .name(methodName) // use unique scenario name among all tests
+  .request((testParameters) => {
+    // Use static hex-encoded data for testing
+    const testData = '0x68656c6c6f20776f726c64'; // "hello world" in hex
+    return http.post(testParameters.RELAY_BASE_URL, getPayLoad(methodName, [testData]), httpParams);
+  })
+  .check(methodName, (r) => isNonErrorResponse(r))
+  .build();
+
+export { options, run };
