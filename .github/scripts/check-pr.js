0dc2454a7a7f87f35227d1fa08d02ca425c14018
Luis Mastrangelo <acuarica@users.noreply.github.com>
2025-07-29 15:33:30 +0200

    chore: use Node's native `fetch` in PR check workflow (#3984)
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- .github/scripts/check-pr.js
+++ .github/scripts/check-pr.js
@@ -1,62 +1,65 @@
-const axios = require('axios');
 
-const githubToken = process.env.GITHUB_TOKEN;
-const { GITHUB_REPOSITORY, GITHUB_PR_NUMBER } = process.env;
+const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_PR_NUMBER } = process.env;
 
 const [owner, repo] = GITHUB_REPOSITORY.split('/');
 
+// https://gist.github.com/leommoore/4526808
+// https://en.wikipedia.org/wiki/ANSI_escape_code
+const dim = text => `\x1b[2m${text}\x1b[0m`;
+const red = text => `\x1b[31m${text}\x1b[0m`;
+const yellow = text => `\x1b[33m${text}\x1b[0m`;
+const blue = text => `\x1b[34m${text}\x1b[0m`;
+
+/**
+ * @param {string} endpoint 
+ * @returns 
+ */
+async function _get(endpoint) {
+  return await fetch('https://api.github.com/repos/' + endpoint, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`,
+    },
+  });
+}
+
+/**
+ * 
+ * @param {string | number} prNumber 
+ * @returns 
+ */
 async function getPRDetails(prNumber) {
-  const url = `https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}`;
-  try {
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `token ${githubToken}`,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    if (error.response && error.response.status === 404) {
-      console.log(`PR #${prNumber} not found in repository ${owner}/${repo}, skipping...`);
-      return null;
-    } else {
-      throw error;
-    }
+  const response = await _get(`${owner}/${repo}/pulls/${prNumber}`);
+  if (response.status === 404) {
+    console.info(dim(`PR #${prNumber} not found in repository ${owner}/${repo}, skipping...`));
+    return null;
   }
+  return await response.json();
 }
 
 async function getIssueDetails(issueOwner, issueRepo, issueNumber) {
-  try {
-    const url = `https://api.github.com/repos/${issueOwner}/${issueRepo}/issues/${issueNumber}`;
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `token ${githubToken}`,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    if (error.response && error.response.status === 404) {
-      console.log(`Issue #${issueNumber} not found in repository ${issueOwner}/${issueRepo}, skipping...`);
-      return null;
-    } else {
-      throw error;
-    }
+  const response = await _get(`${issueOwner}/${issueRepo}/issues/${issueNumber}`);
+  if (response.status === 404) {
+    console.info(dim(`Issue #${issueNumber} not found in repository ${issueOwner}/${issueRepo}, skipping...`));
+    return null;
   }
+  return await response.json();
 }
 
 async function getContributors() {
-  const url = `https://api.github.com/repos/${owner}/${repo}/contributors`;
-  const response = await axios.get(url, {
-    headers: {
-      Authorization: `token ${githubToken}`,
-    },
-  });
-  return response.data;
+  const response = await _get(`${owner}/${repo}/contributors`);
+  return await response.json();
 }
 
+/**
+ * @param {string} text 
+ */
 function stripHTMLTags(text) {
   return text.replace(/<\/?[^>]+(>|$)/g, '');
 }
 
+/**
+ * @param {string} text 
+ */
 function removeCodeBlocks(text) {
   // Remove fenced code blocks (triple backticks or tildes)
   text = text.replace(/```[\s\S]*?```/g, '');
@@ -85,6 +88,9 @@ function extractPRReferences(text) {
   return matches;
 }
 
+/**
+ * @param {string} text 
+ */
 function extractIssueReferences(text) {
   // Regex to match issue references with any number of digits
   // Supports 'Fixes #123', 'owner/repo#123', 'https://github.com/owner/repo/issues/123'
@@ -105,6 +111,9 @@ function extractIssueReferences(text) {
   return issues;
 }
 
+/**
+ * @param {string} text 
+ */
 function cleanText(text) {
   let cleanText = text;
   cleanText = stripHTMLTags(cleanText);
@@ -112,14 +121,19 @@ function cleanText(text) {
   return cleanText;
 }
 
-async function checkPRLabelsAndMilestone(pr) {
-  const { labels: prLabels, milestone: prMilestone } = pr;
+/**
+ * 
+ * @param {*} pr 
+ * @param {string[]} errors
+ */
+async function checkPRLabelsAndMilestone(pr, errors) {
+  const { labels, milestone } = pr;
 
-  if (!prLabels || prLabels.length === 0) {
-    throw new Error('The PR has no labels.');
+  if (!labels || labels.length === 0) {
+    errors.push('The PR has no labels.');
   }
-  if (!prMilestone) {
-    throw new Error('The PR has no milestone.');
+  if (!milestone) {
+    errors.push('The PR has no milestone.');
   }
 }
 
@@ -127,67 +141,79 @@ function isDependabotOrSnykPR(pr) {
   return pr.user.login === 'dependabot[bot]' || pr.user.login === 'swirlds-automation';
 }
 
-async function processIssueReferencesInText(text) {
+/**
+ * @param {string} text 
+ * @param {string[]} errors 
+ */
+async function processIssueReferencesInText(text, errors) {
   const issueReferences = extractIssueReferences(text);
 
-  let hasValidIssueReference = false;
+  let hasElegibleIssueReference = false;
+  let hasErrors = false;
 
-  if (issueReferences.length > 0) {
-    for (const issueRef of issueReferences) {
-      // Only process issues from the same repository
-      if (issueRef.owner === owner && issueRef.repo === repo) {
-        hasValidIssueReference = true;
-        const issue = await getIssueDetails(issueRef.owner, issueRef.repo, issueRef.issueNumber);
-        if (issue) {
-          const { labels: issueLabels, milestone: issueMilestone } = issue;
+  for (const issueRef of issueReferences) {
+    // Only process issues from the same repository
+    if (issueRef.owner === owner && issueRef.repo === repo) {
+      hasElegibleIssueReference = true;
+      const issue = await getIssueDetails(issueRef.owner, issueRef.repo, issueRef.issueNumber);
+      if (issue) {
+        const { labels, milestone } = issue;
 
-          if (!issueLabels || issueLabels.length === 0) {
-            throw new Error(`Associated issue #${issueRef.issueNumber} has no labels.`);
-          }
-          if (!issueMilestone) {
-            throw new Error(`Associated issue #${issueRef.issueNumber} has no milestone.`);
-          }
+        if (!labels || labels.length === 0) {
+          errors.push(`Associated issue #${issueRef.issueNumber} has no labels.`);
+          hasErrors = true;
         }
-      } else {
-        console.log(
-          `Issue #${issueRef.issueNumber} is from a different repository (${issueRef.owner}/${issueRef.repo}), skipping...`,
-        );
+        if (!milestone) {
+          errors.push(`Associated issue #${issueRef.issueNumber} has no milestone.`);
+          hasErrors = true;
+        }
       }
-    }
-
-    if (!hasValidIssueReference) {
-      throw new Error('The PR description must reference at least one issue from the current repository.');
     } else {
-      console.log('All associated issues have labels and milestones.');
+      console.info(
+        dim(`Issue #${issueRef.issueNumber} is from a different repository (${issueRef.owner}/${issueRef.repo}), skipping...`),
+      );
     }
+  }
+
+  if (hasErrors) return;
+  if (!hasElegibleIssueReference) {
+    errors.push('The PR description must reference at least one issue from the current repository.');
   } else {
-    throw new Error('The PR description must reference at least one issue from the current repository.');
+    console.info('All associated issues have labels and milestones.');
   }
 }
 
-async function processPRReferencesInText(text, contributors) {
+/**
+ * @param {string} text 
+ * @param {{login: string}[]} contributors 
+ */
+async function processPRReferencesInText(text, contributors, errors) {
   const prReferences = extractPRReferences(text);
 
   if (prReferences.length === 0) {
-    console.log('No associated PRs found in PR description.');
+    console.info('No associated PRs found in PR description.');
   } else {
     for (const prRef of prReferences) {
       // Only process PRs from the same repository
       if (prRef.owner === owner && prRef.repo === repo) {
-        await processReferencedPR(prRef, contributors);
+        await processReferencedPR(prRef, contributors, errors);
       } else {
-        console.log(`PR #${prRef.prNumber} is from a different repository (${prRef.owner}/${prRef.repo}), skipping...`);
+        console.info(dim(`PR #${prRef.prNumber} is from a different repository (${prRef.owner}/${prRef.repo}), skipping...`));
         // Skip processing issue references from external PRs
       }
     }
   }
 }
 
+/**
+ * @param {string} prRef 
+ * @param {{login: string}[]} contributors 
+ */
 async function processReferencedPR(prRef, contributors) {
   // Attempt to fetch the PR to validate its existence
   const referencedPR = await getPRDetails(prRef.prNumber);
   if (!referencedPR) {
-    console.log(`PR #${prRef.prNumber} does not exist, skipping...`);
+    console.info(dim(`PR #${prRef.prNumber} does not exist, skipping...`));
     return; // Skip if PR not found
   }
 
@@ -196,7 +222,7 @@ async function processReferencedPR(prRef, contributors) {
   const isContributor = contributors.some((contributor) => contributor.login === authorLogin);
 
   if (!isContributor) {
-    console.log(`PR author ${authorLogin} is not a contributor, skipping issue matching for PR #${prRef.prNumber}.`);
+    console.info(dim(`PR author ${authorLogin} is not a contributor, skipping issue matching for PR #${prRef.prNumber}.`));
     return;
   }
 
@@ -207,7 +233,7 @@ async function processReferencedPR(prRef, contributors) {
   const refIssueReferences = extractIssueReferences(refPrBody);
 
   if (refIssueReferences.length === 0) {
-    console.log(`No associated issues found in PR #${prRef.prNumber} description.`);
+    console.info(`No associated issues found in PR #${prRef.prNumber} description.`);
   } else {
     for (const issueRef of refIssueReferences) {
       // Only process issues from the same repository
@@ -217,19 +243,19 @@ async function processReferencedPR(prRef, contributors) {
           const { labels: issueLabels, milestone: issueMilestone } = issue;
 
           if (!issueLabels || issueLabels.length === 0) {
-            throw new Error(`Associated issue #${issueRef.issueNumber} has no labels.`);
+            errors.push(`Associated issue #${issueRef.issueNumber} has no labels.`);
           }
           if (!issueMilestone) {
-            throw new Error(`Associated issue #${issueRef.issueNumber} has no milestone.`);
+            errors.push(`Associated issue #${issueRef.issueNumber} has no milestone.`);
           }
         }
       } else {
-        console.log(
-          `Issue #${issueRef.issueNumber} is from a different repository (${issueRef.owner}/${issueRef.repo}), skipping...`,
+        console.info(
+          dim(`Issue #${issueRef.issueNumber} is from a different repository (${issueRef.owner}/${issueRef.repo}), skipping...`),
         );
       }
     }
-    console.log(`PR #${prRef.prNumber} and all associated issues have labels and milestones.`);
+    console.info(`PR #${prRef.prNumber} and all associated issues have labels and milestones.`);
   }
 }
 
@@ -246,64 +272,67 @@ async function fixSnykPR(pr) {
 
   if (!hasValidPrefix) {
     title = `build(dep): ${title}`;
-    console.log(`Updating PR title to: ${title}`);
-    const url = `https://api.github.com/repos/${owner}/${repo}/pulls/${pr.number}`;
-    await axios.patch(
-      url,
-      { title },
+    console.info(`Updating PR title to: ${title}`);
+    await fetch(
+      `https://api.github.com/repos/${owner}/${repo}/pulls/${pr.number}`,
       {
+        method: 'PATCH',
         headers: {
-          Authorization: `token ${githubToken}`,
+          Authorization: `token ${GITHUB_TOKEN}`,
         },
+        body: JSON.stringify({ title }),
       },
     );
   }
 
   const labelExists = pr.labels && pr.labels.some((label) => label.name.toLowerCase() === 'dependencies');
   if (!labelExists) {
-    console.log("Adding 'dependencies' label to the PR");
+    console.info("Adding 'dependencies' label to the PR");
     // Github API uses /issues both for issues and PRs since they use the same sequence
-    const url = `https://api.github.com/repos/${owner}/${repo}/issues/${pr.number}/labels`;
-    await axios.post(
-      url,
-      { labels: ['dependencies'] },
+    await fetch(`https://api.github.com/repos/${owner}/${repo}/issues/${pr.number}/labels`,
       {
+        method: 'POST',
         headers: {
-          Authorization: `token ${githubToken}`,
+          Authorization: `token ${GITHUB_TOKEN}`,
         },
+        body: JSON.stringify({ labels: ['dependencies'] }),
       },
     );
   }
 }
 
-async function run() {
-  try {
-    const pr = await getPRDetails(GITHUB_PR_NUMBER);
-    if (!pr) {
-      throw new Error(`PR #${GITHUB_PR_NUMBER} not found.`);
-    }
-
-    await fixSnykPR(pr);
-    await checkPRLabelsAndMilestone(pr);
-
-    if (isDependabotOrSnykPR(pr)) {
-      console.log('Dependabot or snyk PR detected. Skipping issue reference requirement.');
-      return;
-    } else {
-      const cleanBody = cleanText(pr.body);
-      await processIssueReferencesInText(cleanBody);
-    }
-
-    const contributors = await getContributors();
-
-    const cleanBody = cleanText(pr.body);
-    await processPRReferencesInText(cleanBody, contributors);
-
-    console.log('All checks completed.');
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+async function main() {
+  const pr = await getPRDetails(GITHUB_PR_NUMBER);
+  if (!pr) {
+    throw new Error(`PR #${GITHUB_PR_NUMBER} not found.`);
   }
+
+  await fixSnykPR(pr);
+  const errors = [];
+
+  await checkPRLabelsAndMilestone(pr, errors);
+
+  if (isDependabotOrSnykPR(pr)) {
+    console.info(dim('Dependabot or snyk PR detected. Skipping issue reference requirement.'));
+    return;
+  }
+
+  const cleanBody = cleanText(pr.body);
+  await processIssueReferencesInText(cleanBody, errors);
+
+  const contributors = await getContributors();
+  await processPRReferencesInText(cleanBody, contributors, errors);
+
+  if (errors.length > 0) {
+    console.info(yellow('PR validation failed with the following errors:'));
+    errors.forEach((error) => console.info(yellow(`- ${error}`)));
+    process.exit(2);
+  }
+
+  console.info(blue('All checks completed.'));
 }
 
-run();
+main().catch((error) => {
+  console.error(red(error.message));
+  process.exit(1);
+});
