846942905391e139480dfaaa78ce962594ccda64
Luis Mastrangelo <luis@swirldslabs.com>
2025-07-31 20:22:43 +0200

    Apply Logan's suggestions
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- packages/server/src/koaJsonRpc/lib/RpcResponse.ts
+++ packages/server/src/koaJsonRpc/lib/RpcResponse.ts
@@ -4,6 +4,15 @@ import { JsonRpcError } from '@hashgraph/json-rpc-relay';
 
 import { IJsonRpcResponse } from './IJsonRpcResponse';
 
+/**
+ * Creates a JSON-RPC response object for a successful request.
+ * It wraps the `result` in a response object with the given ID.
+ * See _Response object_ https://www.jsonrpc.org/specification#response_object for more details.
+ *
+ * @param id - The JSON-RPC ID received in the request or `null` if the request was a notification.
+ * @param result - The result of the response.
+ * @returns A JSON-RPC response object.
+ */
 export function jsonRespResult(id: string | number | null, result: unknown): IJsonRpcResponse {
   if (id !== null && typeof id !== 'string' && typeof id !== 'number') {
     throw new TypeError(`Invalid id type ${typeof id}`);
@@ -16,6 +25,23 @@ export function jsonRespResult(id: string | number | null, result: unknown): IJs
   return { result, jsonrpc: '2.0', id };
 }
 
+/**
+ * Creates a JSON-RPC error object for a `JsonRpcError`.
+ * It wraps the `error` in a response object with the given ID.
+ * See _Error object_ https://www.jsonrpc.org/specification#error_object for more details.
+ *
+ * The returned error message is prefixed with the internal `requestId`.
+ * More specifically, the error message will be formatted as `[Request ID: <requestId>] <error.message>`.
+ *
+ * Users with an error response can report this `requestId` back to Relay operators.
+ * This `requestId` is useful for debugging and troubleshooting purposes.
+ * It can be used to correlate the error with the original request and its corresponding logs.
+ *
+ * @param id - The JSON-RPC ID received in the request or `null` if the request was a notification.
+ * @param error - The error object to include in the response.
+ * @param requestId - The internal request ID used for logging and tracing purposes.
+ * @returns A JSON-RPC error object.
+ */
 export function jsonRespError(id: string | number | null, error: JsonRpcError, requestId: string): IJsonRpcResponse {
   if (id !== null && typeof id !== 'string' && typeof id !== 'number') {
     throw new TypeError(`Invalid id type ${typeof id}`);
