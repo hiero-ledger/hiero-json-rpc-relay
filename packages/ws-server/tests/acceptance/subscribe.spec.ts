728bc26e9facb37253ddb86300237f77ab4aa44a
Luis Mastrangelo <luis@swirldslabs.com>
2025-07-31 05:33:10 +0200

    Fix WS subscribe test
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- packages/ws-server/tests/acceptance/subscribe.spec.ts
+++ packages/ws-server/tests/acceptance/subscribe.spec.ts
@@ -366,8 +366,8 @@ describe('@web-socket-batch-3 eth_subscribe', async function () {
       // wait 500ms to expect the message
       await new Promise((resolve) => setTimeout(resolve, 500));
 
-      expect(response.error.code).to.eq(predefined.UNSUPPORTED_METHOD.code);
-      expect(response.error.message).to.eq(predefined.UNSUPPORTED_METHOD.message);
+      expect(response.error.code).to.be.equal(predefined.UNSUPPORTED_METHOD.code);
+      expect(response.error.message).to.be.equal(requestIdRegex(predefined.UNSUPPORTED_METHOD.message));
 
       // close the connection
       webSocket.close();
