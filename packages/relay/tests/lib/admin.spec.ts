bba36bdd6bb62ebabe7b8b5e5e3d04e396fe18a3
Luis Mastrangelo <luis@swirldslabs.com>
2025-08-02 03:52:01 +0200

    Clean up unused code
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- packages/relay/tests/lib/admin.spec.ts
+++ packages/relay/tests/lib/admin.spec.ts
@@ -5,24 +5,14 @@ import pino from 'pino';
 import { Registry } from 'prom-client';
 
 import { Relay } from '../../src/lib/relay';
-// import { CACHE_LEVEL, CacheService } from '../../src/lib/services/cacheService/cacheService';
 import { RequestDetails } from '../../src/lib/types';
 import { withOverriddenEnvsInMochaTest } from '../helpers';
 
 const logger = pino({ level: 'silent' });
-let relay;
+let relay: Relay;
 
 const requestDetails = new RequestDetails({ requestId: 'admin', ipAddress: '0.0.0.0' });
 describe('Admin', async function () {
-  // this.beforeAll(() => {
-  //   // @ts-ignore
-  //   CacheService.instances = [];
-  // });
-
-  // this.beforeEach(async () => {
-  //   await CacheService.getInstance(CACHE_LEVEL.L1).clear(requestDetails);
-  // });
-
   it('should execute config', async () => {
     relay = new Relay(logger, new Registry());
     const res = await relay.admin().config(requestDetails);
