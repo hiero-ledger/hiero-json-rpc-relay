e053263ba37457887cfdb352178721fb321a6ba7
Simeon Nakov <simeon.nakov@limechain.tech>
2025-08-15 15:27:13 +0300

    reduced code repetition in test file
    Signed-off-by: Simeon Nakov <simeon.nakov@limechain.tech>

--- packages/relay/tests/lib/factories/transactionFactory.spec.ts
+++ packages/relay/tests/lib/factories/transactionFactory.spec.ts
@@ -6,6 +6,52 @@ import { createTransactionFromContractResult } from '../../../src/lib/factories/
 
 describe('TransactionFactory', () => {
   describe('createTransactionFromContractResult', () => {
+    const expectFormattedResult = (
+      formattedResult: any,
+      expectedValues: {
+        blockNumber?: string | null;
+        r?: string;
+        s?: string;
+        gas?: string;
+        gasPrice?: string;
+        maxPriorityFeePerGas?: string;
+        maxFeePerGas?: string;
+        nonce?: string;
+        transactionIndex?: string | null;
+        v?: string;
+        yParity?: string;
+        value?: string;
+      },
+    ) => {
+      expect(formattedResult.accessList).to.deep.eq([]);
+      expect(formattedResult.blockHash).to.equal('0xb0f10139fa0bf9e66402c8c0e5ed364e07cf83b3726c8045fabf86a07f488713');
+      expect(formattedResult.blockNumber).to.equal(
+        'blockNumber' in expectedValues ? expectedValues.blockNumber : '0x210',
+      );
+      expect(formattedResult.chainId).to.equal('0x12a');
+      expect(formattedResult.from).to.equal('0x05fba803be258049a27b820088bab1cad2058871');
+      expect(formattedResult.gas).to.equal(expectedValues.gas ?? '0x61a80');
+      expect(formattedResult.gasPrice).to.equal(expectedValues.gasPrice ?? '0x0');
+      expect(formattedResult.hash).to.equal('0xfc4ab7133197016293d2e14e8cf9c5227b07357e6385184f1cd1cb40d783cfbd');
+      expect(formattedResult.input).to.equal('0x08090033');
+      expect(formattedResult.maxPriorityFeePerGas).to.equal(expectedValues.maxPriorityFeePerGas ?? '0x0');
+      expect(formattedResult.maxFeePerGas).to.equal(expectedValues.maxFeePerGas ?? '0x59');
+      expect(formattedResult.nonce).to.equal(expectedValues.nonce ?? '0x2');
+      expect(formattedResult.r).to.equal(
+        expectedValues.r ?? '0x2af9d41244c702764ed86c5b9f1a734b075b91c4d9c65e78bc584b0e35181e42',
+      );
+      expect(formattedResult.s).to.equal(
+        expectedValues.s ?? '0x3f0a6baa347876e08c53ffc70619ba75881841885b2bd114dbb1905cd57112a5',
+      );
+      expect(formattedResult.to).to.equal('0x0000000000000000000000000000000000000409');
+      expect(formattedResult.transactionIndex).to.equal(
+        'transactionIndex' in expectedValues ? expectedValues.transactionIndex : '0x9',
+      );
+      expect(formattedResult.type).to.equal('0x2');
+      expect(formattedResult.yParity).to.equal(expectedValues.yParity ?? '0x1');
+      expect(formattedResult.v).to.equal(expectedValues.v ?? '0x1');
+      expect(formattedResult.value).to.equal(expectedValues.value ?? '0x0');
+    };
     const contractResult = {
       amount: 0,
       from: '0x05fba803be258049a27b820088bab1cad2058871',
@@ -28,24 +74,9 @@ describe('TransactionFactory', () => {
     };
 
     const contractResultZeroPrefixedSignatureS = {
-      amount: 0,
-      from: '0x05fba803be258049a27b820088bab1cad2058871',
-      function_parameters: '0x08090033',
-      gas_used: 400000,
-      to: '0x0000000000000000000000000000000000000409',
-      hash: '0xfc4ab7133197016293d2e14e8cf9c5227b07357e6385184f1cd1cb40d783cfbd',
-      block_hash: '0xb0f10139fa0bf9e66402c8c0e5ed364e07cf83b3726c8045fabf86a07f4887130e4650cb5cf48a9f6139a805b78f0312',
-      block_number: 528,
-      transaction_index: 9,
-      chain_id: '0x12a',
-      gas_price: '0x',
-      max_fee_per_gas: '0x59',
-      max_priority_fee_per_gas: '0x',
+      ...contractResult,
       r: '0x58075c8984de34a46c9617ab2b4e0ed5ddc8803e718c42152ed5d58b82166676',
       s: '0x0dd3a5aeb203d9284e50a9973bc5e266a3ea66da1fbb793b244b19b42f19e00b',
-      type: 2,
-      v: 1,
-      nonce: 2,
     };
 
     it('should return null if null is passed', () => {
@@ -54,50 +85,15 @@ describe('TransactionFactory', () => {
 
     it('should return a valid match', () => {
       const formattedResult: any = createTransactionFromContractResult(contractResult);
-      expect(formattedResult.accessList).to.deep.eq([]);
-      expect(formattedResult.blockHash).to.equal('0xb0f10139fa0bf9e66402c8c0e5ed364e07cf83b3726c8045fabf86a07f488713');
-      expect(formattedResult.blockNumber).to.equal('0x210');
-      expect(formattedResult.chainId).to.equal('0x12a');
-      expect(formattedResult.from).to.equal('0x05fba803be258049a27b820088bab1cad2058871');
-      expect(formattedResult.gas).to.equal('0x61a80');
-      expect(formattedResult.gasPrice).to.equal('0x0');
-      expect(formattedResult.hash).to.equal('0xfc4ab7133197016293d2e14e8cf9c5227b07357e6385184f1cd1cb40d783cfbd');
-      expect(formattedResult.input).to.equal('0x08090033');
-      expect(formattedResult.maxPriorityFeePerGas).to.equal('0x0');
-      expect(formattedResult.maxFeePerGas).to.equal('0x59');
-      expect(formattedResult.nonce).to.equal('0x2');
-      expect(formattedResult.r).to.equal('0x2af9d41244c702764ed86c5b9f1a734b075b91c4d9c65e78bc584b0e35181e42');
-      expect(formattedResult.s).to.equal('0x3f0a6baa347876e08c53ffc70619ba75881841885b2bd114dbb1905cd57112a5');
-      expect(formattedResult.to).to.equal('0x0000000000000000000000000000000000000409');
-      expect(formattedResult.transactionIndex).to.equal('0x9');
-      expect(formattedResult.type).to.equal('0x2');
-      expect(formattedResult.yParity).to.equal('0x1');
-      expect(formattedResult.v).to.equal(`0x1`);
-      expect(formattedResult.value).to.equal('0x0');
+      expectFormattedResult(formattedResult, {});
     });
 
     it('should return a valid signature s value', () => {
       const formattedResult: any = createTransactionFromContractResult(contractResultZeroPrefixedSignatureS);
-      expect(formattedResult.accessList).to.deep.eq([]);
-      expect(formattedResult.blockHash).to.equal('0xb0f10139fa0bf9e66402c8c0e5ed364e07cf83b3726c8045fabf86a07f488713');
-      expect(formattedResult.blockNumber).to.equal('0x210');
-      expect(formattedResult.chainId).to.equal('0x12a');
-      expect(formattedResult.from).to.equal('0x05fba803be258049a27b820088bab1cad2058871');
-      expect(formattedResult.gas).to.equal('0x61a80');
-      expect(formattedResult.gasPrice).to.equal('0x0');
-      expect(formattedResult.hash).to.equal('0xfc4ab7133197016293d2e14e8cf9c5227b07357e6385184f1cd1cb40d783cfbd');
-      expect(formattedResult.input).to.equal('0x08090033');
-      expect(formattedResult.maxPriorityFeePerGas).to.equal('0x0');
-      expect(formattedResult.maxFeePerGas).to.equal('0x59');
-      expect(formattedResult.nonce).to.equal('0x2');
-      expect(formattedResult.r).to.equal('0x58075c8984de34a46c9617ab2b4e0ed5ddc8803e718c42152ed5d58b82166676');
-      expect(formattedResult.s).to.equal('0xdd3a5aeb203d9284e50a9973bc5e266a3ea66da1fbb793b244b19b42f19e00b');
-      expect(formattedResult.to).to.equal('0x0000000000000000000000000000000000000409');
-      expect(formattedResult.transactionIndex).to.equal('0x9');
-      expect(formattedResult.type).to.equal('0x2');
-      expect(formattedResult.yParity).to.equal('0x1');
-      expect(formattedResult.v).to.equal(`0x1`);
-      expect(formattedResult.value).to.equal('0x0');
+      expectFormattedResult(formattedResult, {
+        r: '0x58075c8984de34a46c9617ab2b4e0ed5ddc8803e718c42152ed5d58b82166676',
+        s: '0xdd3a5aeb203d9284e50a9973bc5e266a3ea66da1fbb793b244b19b42f19e00b',
+      });
     });
 
     it('should return nullable fields', () => {
@@ -115,24 +111,17 @@ describe('TransactionFactory', () => {
         v: null,
         value: null,
       });
-      expect(formattedResult.blockHash).to.equal('0xb0f10139fa0bf9e66402c8c0e5ed364e07cf83b3726c8045fabf86a07f488713');
-      expect(formattedResult.blockNumber).to.equal(null);
-      expect(formattedResult.chainId).to.equal('0x12a');
-      expect(formattedResult.from).to.equal('0x05fba803be258049a27b820088bab1cad2058871');
-      expect(formattedResult.gas).to.equal('0x0');
-      expect(formattedResult.gasPrice).to.equal('0x0');
-      expect(formattedResult.hash).to.equal('0xfc4ab7133197016293d2e14e8cf9c5227b07357e6385184f1cd1cb40d783cfbd');
-      expect(formattedResult.input).to.equal('0x08090033');
-      expect(formattedResult.maxPriorityFeePerGas).to.equal('0x0');
-      expect(formattedResult.maxFeePerGas).to.equal('0x0');
-      expect(formattedResult.nonce).to.equal('0x0');
-      expect(formattedResult.r).to.equal('0x0');
-      expect(formattedResult.s).to.equal('0x0');
-      expect(formattedResult.to).to.equal('0x0000000000000000000000000000000000000409');
-      expect(formattedResult.transactionIndex).to.equal(null);
-      expect(formattedResult.v).to.equal(`0x0`);
-      expect(formattedResult.yParity).to.equal('0x0');
-      expect(formattedResult.value).to.equal('0x0');
+      expectFormattedResult(formattedResult, {
+        blockNumber: null,
+        gas: '0x0',
+        maxFeePerGas: '0x0',
+        nonce: '0x0',
+        r: '0x0',
+        s: '0x0',
+        transactionIndex: null,
+        v: '0x0',
+        yParity: '0x0',
+      });
     });
 
     it('Should not include chainId field for legacy EIP155 transaction (tx.chainId=0x0)', () => {
