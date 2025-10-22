5367c85e2591e2b2ac7e3e3fc08cacb83e517188
Logan Nguyen <logan.nguyen@swirldslabs.com>
2025-10-22 11:24:20 -0500

    setimtout poc --- revert me
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- packages/relay/src/lib/services/lockService/LocalLockStrategy.ts
+++ packages/relay/src/lib/services/lockService/LocalLockStrategy.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@hashgraph/json-rpc-config-service/dist/services'
 import { randomUUID } from 'crypto';
 import { LRUCache } from 'lru-cache';
 import { Logger } from 'pino';
+import { Mutex, MutexInterface, withTimeout } from 'async-mutex';
 
 import { LockStrategy } from './LockStrategy';
 
@@ -46,6 +47,8 @@ export class LocalLockStrategy implements LockStrategy {
    */
   private readonly sessionQueues: Map<string, string[]>;
 
+  private readonly locksMap: LRUCache<string, any>;
+
   /**
    * Creates a new LocalLockStrategy instance.
    *
@@ -57,6 +60,10 @@ export class LocalLockStrategy implements LockStrategy {
     this.logger.info(
       `Local lock strategy initialized: lockAcquisitionTimeoutMs=${this.lockAcquisitionTimeoutMs}ms, lockTtlMs=${this.lockTtlMs}ms, acquisitionPollIntervalMs=${this.acquisitionPollIntervalMs}ms`,
     );
+
+    this.locksMap = new LRUCache<string, any>({
+      max: 1000,
+    });
   }
 
   /**
@@ -70,66 +77,99 @@ export class LocalLockStrategy implements LockStrategy {
     const sessionKey = randomUUID();
     const waitStartedAt = Date.now();
 
-    // Get or create session queue for this lock
-    let sessionQueue = this.sessionQueues.get(lockKey);
-    if (!sessionQueue) {
-      sessionQueue = [];
-      this.sessionQueues.set(lockKey, sessionQueue);
-    }
+    let mutex: Mutex;
 
     try {
-      // Enqueue the session key
-      sessionQueue.unshift(sessionKey);
-      this.logger.debug(`New item joined lock queue: lockId=${lockId}, session=${sessionKey}`);
-
-      // Poll until first in queue to acquire the lock, or until exceeding timeout
-      while (Date.now() - waitStartedAt < this.lockAcquisitionTimeoutMs) {
-        const firstInQueue = sessionQueue[sessionQueue.length - 1];
-
-        // Check if this session is first in queue and lock is not held
-        if (firstInQueue === sessionKey && !this.lockStorage.has(lockKey)) {
-          // Acquire lock only when lock does not exist
-          // This ensures only one client can hold the lock at a time
-          this.lockStorage.set(lockKey, sessionKey);
-
-          const waitDurationMs = Date.now() - waitStartedAt;
-          this.logger.debug(`Local lock acquired: ${lockId}, waited ${waitDurationMs}ms, session: ${sessionKey}`);
-
-          return sessionKey;
-        }
-
-        // Wait before next poll
-        await new Promise((resolve) => setTimeout(resolve, this.acquisitionPollIntervalMs));
-      }
-
-      // Lock acquisition process timed out
-      throw new Error('Lock acquisition timeout');
-    } catch (error) {
-      const waitDurationMs = Date.now() - waitStartedAt;
-      if (error instanceof Error && error.message.includes('timeout')) {
-        this.logger.warn(`Lock acquisition timeout: ${lockId}, waited ${waitDurationMs}ms, session: ${sessionKey}`);
+      if (this.locksMap.has(lockKey)) {
+        mutex = this.locksMap.get(lockKey)!;
       } else {
-        this.logger.warn(
-          `Unexpected error during local lock acquisition for ${lockId} after ${waitDurationMs}ms for session ${sessionKey}`,
-          error,
-        );
+        mutex = new Mutex();
+        this.locksMap.set(lockKey, mutex);
       }
 
-      // Return null to signal that the lock was not acquired, allowing other processes
-      // to continue without interruption instead of being blocked by an exception.
-      return null;
-    } finally {
-      // Remove session key from queue regardless of success or failure
-      const sessionKeyIndex = sessionQueue.indexOf(sessionKey);
-      if (sessionKeyIndex !== -1) {
-        sessionQueue.splice(sessionKeyIndex, 1);
-      }
+      let isTimeout = false;
 
-      // Automatically deletes the queue entry if it becomes empty after removal - avoid accumulation
-      if (sessionQueue.length === 0) {
-        this.sessionQueues.delete(lockKey);
+      const handler = setTimeout(() => {
+        isTimeout = true;
+        mutex.release();
+        console.log(`Deleted mutex for ${lockKey} from locksMap}`);
+      }, 9000);
+
+      console.log(`Attempting to acquire mutex lock for ${lockKey}}`);
+      await mutex.acquire();
+      this.lockStorage.set(lockKey, sessionKey);
+      console.log(`Mutex lock acquired for ${lockKey}}`);
+      if (!isTimeout) {
+        console.log(`Clearing timeout handler for ${lockKey}}`);
+        clearTimeout(handler);
+      } else {
+        console.log(`Mutex lock acquisition timed out for ${lockKey}}`);
       }
+    } catch (error) {
+      console.error(`Failed to acquire mutex lock within timeout}:`, error);
     }
+
+    return sessionKey;
+
+    // // Get or create session queue for this lock
+    // let sessionQueue = this.sessionQueues.get(lockKey);
+    // if (!sessionQueue) {
+    //   sessionQueue = [];
+    //   this.sessionQueues.set(lockKey, sessionQueue);
+    // }
+    // try {
+    //   // Enqueue the session key
+    //   sessionQueue.unshift(sessionKey);
+    //   this.logger.debug(`New item joined lock queue: lockId=${lockId}, session=${sessionKey}`);
+
+    //   // Poll until first in queue to acquire the lock, or until exceeding timeout
+    //   while (Date.now() - waitStartedAt < this.lockAcquisitionTimeoutMs) {
+    //     const firstInQueue = sessionQueue[sessionQueue.length - 1];
+
+    //     // Check if this session is first in queue and lock is not held
+    //     if (firstInQueue === sessionKey && !this.lockStorage.has(lockKey)) {
+    //       // Acquire lock only when lock does not exist
+    //       // This ensures only one client can hold the lock at a time
+    //       this.lockStorage.set(lockKey, sessionKey);
+
+    //       const waitDurationMs = Date.now() - waitStartedAt;
+    //       this.logger.debug(`Local lock acquired: ${lockId}, waited ${waitDurationMs}ms, session: ${sessionKey}`);
+
+    //       return sessionKey;
+    //     }
+
+    //     // Wait before next poll
+    //     await new Promise((resolve) => setTimeout(resolve, this.acquisitionPollIntervalMs));
+    //   }
+
+    //   // Lock acquisition process timed out
+    //   throw new Error('Lock acquisition timeout');
+    // } catch (error) {
+    //   const waitDurationMs = Date.now() - waitStartedAt;
+    //   if (error instanceof Error && error.message.includes('timeout')) {
+    //     this.logger.warn(`Lock acquisition timeout: ${lockId}, waited ${waitDurationMs}ms, session: ${sessionKey}`);
+    //   } else {
+    //     this.logger.warn(
+    //       `Unexpected error during local lock acquisition for ${lockId} after ${waitDurationMs}ms for session ${sessionKey}`,
+    //       error,
+    //     );
+    //   }
+
+    //   // Return null to signal that the lock was not acquired, allowing other processes
+    //   // to continue without interruption instead of being blocked by an exception.
+    //   return null;
+    // } finally {
+    //   // Remove session key from queue regardless of success or failure
+    //   const sessionKeyIndex = sessionQueue.indexOf(sessionKey);
+    //   if (sessionKeyIndex !== -1) {
+    //     sessionQueue.splice(sessionKeyIndex, 1);
+    //   }
+
+    //   // Automatically deletes the queue entry if it becomes empty after removal - avoid accumulation
+    //   if (sessionQueue.length === 0) {
+    //     this.sessionQueues.delete(lockKey);
+    //   }
+    // }
   }
 
   /**
@@ -146,17 +186,31 @@ export class LocalLockStrategy implements LockStrategy {
     const lockKey = this.buildLockKey(lockId);
     const currenSessionKey = this.lockStorage.get(lockKey);
 
-    // Ensure the lock is still valid and owned by the current session before releasing,
-    // preventing double-release or invalid session releases.
+    const mutex = this.locksMap.get(lockKey)!;
+    console.log(`Using release existing mutex for ${lockKey}}`);
+
     if (currenSessionKey === sessionKey) {
       try {
         // Delete lock entry to release the lock
         this.lockStorage.delete(lockKey);
-        this.logger.debug(`Local lock released: ${lockId}, session: ${sessionKey}`);
+        mutex.release();
+        console.log(`Mutex release lock released for ${lockKey}}`);
       } catch (error) {
-        this.logger.warn(`Error releasing local lock for ${lockId}:`, error);
+        console.error(`Failed release to release mutex lock for ${lockKey}}:`, error);
       }
     }
+
+    // Ensure the lock is still valid and owned by the current session before releasing,
+    // preventing double-release or invalid session releases.
+    // if (currenSessionKey === sessionKey) {
+    //   try {
+    //     // Delete lock entry to release the lock
+    //     this.lockStorage.delete(lockKey);
+    //     this.logger.debug(`Local lock released: ${lockId}, session: ${sessionKey}`);
+    //   } catch (error) {
+    //     this.logger.warn(`Error releasing local lock for ${lockId}:`, error);
+    //   }
+    // }
   }
 
   /**
