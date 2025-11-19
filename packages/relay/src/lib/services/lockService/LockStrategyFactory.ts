6ae725a86a0ce2506d85dec82ab295a2a5ed339d
Nikolay Atanasow <n.atanasow94@gmail.com>
2025-11-19 22:18:15 +0200

    feat: create `LocalLockStrategy` using LRU and async-mutex (#4610)
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- packages/relay/src/lib/services/lockService/LockStrategyFactory.ts
+++ packages/relay/src/lib/services/lockService/LockStrategyFactory.ts
@@ -3,7 +3,8 @@
 import { Logger } from 'pino';
 import { RedisClientType } from 'redis';
 
-import { LockStrategy } from '../../types/lock';
+import { LockStrategy } from '../../types';
+import { LocalLockStrategy } from './LocalLockStrategy';
 
 /**
  * Factory for creating LockStrategy instances.
@@ -20,13 +21,13 @@ export class LockStrategyFactory {
    * @param logger - Logger instance for the lock strategy.
    * @returns A LockStrategy implementation.
    */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   static create(redisClient: RedisClientType | undefined, logger: Logger): LockStrategy {
     // TODO: Remove placeholder errors once strategies are implemented
     if (redisClient) {
-      throw new Error('Redis lock strategy not yet implemented');
+      // throw new Error('Redis lock strategy not yet implemented');
     }
 
-    throw new Error('Local lock strategy not yet implemented');
+    return new LocalLockStrategy(logger);
   }
 }
