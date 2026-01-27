1c4b168a39c9923bb4dc6e7a1457b5ddafd10dbe
Logan Nguyen <logan.nguyen@swirldslabs.com>
2026-01-27 14:21:45 -0600

    fix: added custom validation for MIRROR_NODE_MAX_LOGS_PER_TIMESTAMP_SLICE
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- packages/config-service/src/services/index.ts
+++ packages/config-service/src/services/index.ts
@@ -64,6 +64,12 @@ export class ConfigService {
     }
 
     this.validateReadOnlyMode();
+
+    // note: temporary bandage solution
+    // should be replaced after https://github.com/hiero-ledger/hiero-json-rpc-relay/issues/4840 is implemented
+    if (this.get('MIRROR_NODE_MAX_LOGS_PER_TIMESTAMP_SLICE') === 0) {
+      throw new Error('MIRROR_NODE_MAX_LOGS_PER_TIMESTAMP_SLICE cannot be zero.');
+    }
   }
 
   /**
