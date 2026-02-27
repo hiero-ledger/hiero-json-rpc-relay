cd672cb8f07c0708d89398abea6b31d23fbe99b2
Mariusz Jasuwienas <jasuwienas@gmail.com>
2026-02-27 14:48:13 +0100

    feat: retain title description keys (#4915)
    Signed-off-by: Mariusz Jasuwienas <jasuwienas@gmail.com>

--- scripts/openrpc-json-updater/utils/merge.utils.js
+++ scripts/openrpc-json-updater/utils/merge.utils.js
@@ -121,6 +121,13 @@ export function setObjectByPath(obj, path, value) {
   }
 
   const lastPart = parts[parts.length - 1];
+
+  ['description', 'title'].forEach((key) => {
+    if (current[lastPart][key] && !value[key]) {
+      value[key] = current[lastPart][key];
+    }
+  });
+
   current[lastPart] = value;
 }
 
