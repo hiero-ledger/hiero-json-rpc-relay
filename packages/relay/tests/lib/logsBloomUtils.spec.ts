6cc945b378a7300a547eb092d61a2913e92d13a1
Mariusz Jasuwienas <jasuwienas@gmail.com>
2026-03-05 12:39:48 +0100

    feat: return complete logs for synth tx (#5043)
    Signed-off-by: Mariusz Jasuwienas <jasuwienas@gmail.com>

--- packages/relay/tests/lib/logsBloomUtils.spec.ts
+++ packages/relay/tests/lib/logsBloomUtils.spec.ts
@@ -5,7 +5,15 @@ import { keccak256, ZeroAddress } from 'ethers';
 
 import { strip0x } from '../../src/formatters';
 import constants from '../../src/lib/constants';
+import { Log } from '../../src/lib/model';
 import { LogsBloomUtils } from '../../src/logsBloomUtils';
+
+/**
+ * Helper function to create the Logs array
+ * @param address
+ * @param topics
+ */
+const toLogs = (address: string, topics?: string[] | null) => [{ address, topics }] as Log[];
 
 describe('LogsBloomUtils', () => {
   describe('buildLogsBloom', () => {
@@ -23,7 +31,7 @@ describe('LogsBloomUtils', () => {
         const first2bytes = new DataView(itemBuf.buffer).getUint16(i * 2);
         const loc = LogsBloomUtils.MASK & first2bytes;
         const byteLoc = loc >> 3;
-        const bitLoc = 1 << loc % 8;
+        const bitLoc = 1 << (loc % 8);
         match = (bitvectorUint8Arr[LogsBloomUtils.BYTE_SIZE - byteLoc - 1] & bitLoc) !== 0;
       }
 
@@ -47,40 +55,47 @@ describe('LogsBloomUtils', () => {
 
     it('should be able to generate emptyBloom if passed address is undefined', () => {
       // @ts-ignore
-      const res = LogsBloomUtils.buildLogsBloom(undefined, topics);
+      const res = LogsBloomUtils.buildLogsBloom(toLogs(undefined, topics));
       expect(emptyBloom).to.equal(res);
     });
 
     it('should be able to generate emptyBloom if passed address is null', () => {
       // @ts-ignore
-      const res = LogsBloomUtils.buildLogsBloom(null, topics);
+      const res = LogsBloomUtils.buildLogsBloom(toLogs(null, topics));
       expect(emptyBloom).to.equal(res);
     });
 
-    it('should be able to generate emptyBloom if passed topics value is undefined', () => {
-      // @ts-ignore
-      const res = LogsBloomUtils.buildLogsBloom(address, undefined);
-      expect(emptyBloom).to.equal(res);
+    [
+      { label: 'undefined', value: undefined },
+      { label: 'null', value: null },
+      { label: 'empty array', value: [] },
+    ].forEach(({ label, value }) => {
+      it(`should be able to generate a bloom if passed topics value is ${label}`, () => {
+        const res = LogsBloomUtils.buildLogsBloom(toLogs(address, value));
+        expect(res).to.not.equal(emptyBloom);
+        const bytes = res.slice(2).match(/.{2}/g)!;
+        expect([bytes[132], bytes[159], bytes[195]]).to.deep.equal(['04', '01', '01']);
+      });
     });
 
-    it('should be able to generate emptyBloom if passed topics value is null', () => {
+    it('should be able to generate a bloom if passed topics value is null', () => {
       // @ts-ignore
-      const res = LogsBloomUtils.buildLogsBloom(address, null);
-      expect(emptyBloom).to.equal(res);
+      const res = LogsBloomUtils.buildLogsBloom(toLogs(address, null));
+      expect(res).to.not.equal(emptyBloom);
+    });
+
+    it('should be able to generate a emptyBloom if there are no logs', () => {
+      const res = LogsBloomUtils.buildLogsBloom(toLogs(address, []));
+      expect(res).to.not.equal(emptyBloom);
     });
 
     it('should be able to generate emptyBloom if address is empty', () => {
-      const res = LogsBloomUtils.buildLogsBloom('', topics);
-      expect(emptyBloom).to.equal(res);
-    });
-
-    it('should be able to generate emptyBloom if there are no logs', () => {
-      const res = LogsBloomUtils.buildLogsBloom(address, []);
-      expect(emptyBloom).to.equal(res);
+      const res = LogsBloomUtils.buildLogsBloom(toLogs('', topics));
+      expect(res).to.equal(emptyBloom);
     });
 
     it('should be able to generate logsBloom of transfer event', () => {
-      const res = LogsBloomUtils.buildLogsBloom(address, topics);
+      const res = LogsBloomUtils.buildLogsBloom(toLogs(address, topics));
       expect(expectedLogsBloom).to.equal(res);
     });
 
@@ -97,6 +112,67 @@ describe('LogsBloomUtils', () => {
       expect(
         checkInLogsBloom('0x0000000000000000000000C70c3C06A4db619B7879d060B9215d528F584FcC', expectedLogsBloom),
       ).to.equal(false);
+    });
+
+    it('should be able to generate a bloom for multiple logs', () => {
+      // Test fixture was taken from an Ethereum mainnet transaction receipt (logs + logsBloom)
+      // and trimmed to only the fields needed for bloom calculation.
+      // Source: https://etherscan.io/tx/0x3766f3eccfdd6c8ad9fde50f6496dcc15a0988063e5493651d6f4fdbe7590aa5
+      const transactionReceipt = {
+        logs: [
+          {
+            address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+            topics: [
+              '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+              '0x000000000000000000000000cdf330ffe8b2233c96576a61813d6ecd9c3ff700',
+              '0x00000000000000000000000088e6a0c2ddd26feeb64f039a2c41296fcb3f5640',
+            ],
+          },
+          {
+            address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+            topics: [
+              '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+              '0x000000000000000000000000cdf330ffe8b2233c96576a61813d6ecd9c3ff700',
+              '0x00000000000000000000000088e6a0c2ddd26feeb64f039a2c41296fcb3f5640',
+            ],
+          },
+          {
+            address: '0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640',
+            topics: [
+              '0x7a53080ba414158be7ec69b987b5fb7d07dee101fe85488f0853ae16239d0bde',
+              '0x000000000000000000000000c36442b4a4522e871399cd717abdd847ab11fe88',
+              '0x000000000000000000000000000000000000000000000000000000000002fdfa',
+              '0x0000000000000000000000000000000000000000000000000000000000030052',
+            ],
+          },
+          {
+            address: '0xc36442b4a4522e871399cd717abdd847ab11fe88',
+            topics: [
+              '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+              '0x0000000000000000000000000000000000000000000000000000000000000000',
+              '0x000000000000000000000000cdf330ffe8b2233c96576a61813d6ecd9c3ff700',
+              '0x000000000000000000000000cdf330ffe8b2233c96576a61813d6ecd9c3ff700',
+            ],
+          },
+          {
+            address: '0xc36442b4a4522e871399cd717abdd847ab11fe88',
+            topics: [
+              '0x3067048beee31b25b2f1681f88dac838c8bba36af25bfb2b7cf7473a5847e35f',
+              '0x0000000000000000000000000000000000000000000000000000000000035cbf',
+            ],
+          },
+        ],
+        logsBloom:
+          '0x00800000010000000000100000000000000000000000000000000000840000000000000400000000000008000000' +
+          '00000201000008000000840000000000000000000000200080000800001800000000400000800000000000080800' +
+          '00000000000000000200000000001000000008000000000008000000000000100000000000000000080000000000' +
+          '00000000000000000000010000000000000000000000000000000000200000000000000000000000000000200000' +
+          '00200000000808004000000200000008000008000000000000000000004000000000000000002000000020000000' +
+          '0000000010000000000000000000000000000000000000000800',
+      };
+
+      const res = LogsBloomUtils.buildLogsBloom(transactionReceipt.logs as Log[]);
+      expect(res).to.equal(transactionReceipt.logsBloom);
     });
   });
 });
