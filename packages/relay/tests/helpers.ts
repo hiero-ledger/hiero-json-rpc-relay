b900b32d38261425015935ff37b5423140a60a91
Logan Nguyen <logan.nguyen@swirldslabs.com>
2026-04-08 12:55:02 -0500

    feat: move getBalance to specific worker #5146 (#5232)
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>
    Co-authored-by: Thomas Boot <267065272+bootcodes@users.noreply.github.com>

--- packages/relay/tests/helpers.ts
+++ packages/relay/tests/helpers.ts
@@ -1147,6 +1147,7 @@ export const mockWorkersPool = async (mirrorNodeInstance, commonService, cacheSe
   };
   const blockWorker = proxyquire('../../relay/dist/lib/services/ethService/blockService/blockWorker.js', deps);
   const commonWorker = proxyquire('../../relay/dist/lib/services/ethService/ethCommonService/commonWorker.js', deps);
+  const accountWorker = proxyquire('../../relay/dist/lib/services/ethService/accountService/accountWorker.js', deps);
 
   WorkersPool['instance'] = {
     run: async (task: any) => {
@@ -1178,6 +1179,8 @@ export const mockWorkersPool = async (mirrorNodeInstance, commonService, cacheSe
               task.topics,
               task.requestDetails,
             );
+          case 'getBalance':
+            return await accountWorker.getBalance(task.account, task.blockNumberOrTagOrHash, task.requestDetails);
           default:
             throw new Error(`Unsupported task type ${task.type}`);
         }
