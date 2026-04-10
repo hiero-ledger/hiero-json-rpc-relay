8243136f54cc78a7ed3615fe49d4801a31e4cb85
Thomas Boot <thomas.boot@swirldslabs.com>
2026-04-10 10:46:05 +0200

    chore : fix `printEnvs`
    Signed-off-by: Thomas Boot <thomas.boot@swirldslabs.com>

--- src/config-service/commands/printEnvs.ts
+++ src/config-service/commands/printEnvs.ts
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { ConfigService } from '../services';
+// Need to keep like this to load from compiled output
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { ConfigService } = require('../../../dist/config-service/services');
 
 // @ts-ignore
 ConfigService.getInstance();
