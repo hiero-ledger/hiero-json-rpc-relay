6374fd764356464071f9f9c36c79c2c6fe3b3963
Thomas Boot <thomas.boot@swirldslabs.com>
2026-04-10 14:53:10 +0200

    chore: fix relative import
    Signed-off-by: Thomas Boot <thomas.boot@swirldslabs.com>

--- tests/protocol/index.spec.ts
+++ tests/protocol/index.spec.ts
@@ -3,7 +3,7 @@
 // Important! Load env variables before importing anything else
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 import { registerAcceptanceSuite } from '../acceptance-runner';
 
