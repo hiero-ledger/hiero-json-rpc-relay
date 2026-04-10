eb5afc7a1af123e3f533ae3427c11f6e04a21214
Thomas Boot <thomas.boot@swirldslabs.com>
2026-04-10 15:04:43 +0200

    chore: fix codacy issues on index.spec.ts
    Signed-off-by: Thomas Boot <thomas.boot@swirldslabs.com>

--- tests/protocol/index.spec.ts
+++ tests/protocol/index.spec.ts
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Important! Load env variables before importing anything else
+import path from 'node:path';
+
 import dotenv from 'dotenv';
-import path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 import { registerAcceptanceSuite } from '../acceptance-runner';
