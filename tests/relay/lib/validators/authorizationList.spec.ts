f1f40b431a940c8bfe8bd08dbaf056f1c8a7cf10
Thomas Boot <thomas.boot@swirldslabs.com>
2026-04-15 12:26:52 +0200

    chore: add shared authorizationList validator
    Signed-off-by: Thomas Boot <thomas.boot@swirldslabs.com>

---
+++ tests/relay/lib/validators/authorizationList.spec.ts
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: Apache-2.0
+import { expect } from 'chai';
+
+import type { AuthorizationListEntry } from '../../../../src/relay/lib/model';
+import { validateAuthorizationList } from '../../../../src/relay/lib/validators/authorizationList';
+import { contractAddress1 } from '../../../helpers';
+
+describe('validateAuthorizationList', () => {
+  const authEntry = {
+    chainId: '0x12a',
+    address: contractAddress1,
+    nonce: '0x0',
+    r: '0x' + 'aa'.repeat(32),
+    s: '0x' + 'bb'.repeat(32),
+    yParity: '0x0',
+  } as unknown as AuthorizationListEntry;
+
+  describe('non-type-4 transactions', () => {
+    it('should not throw for type 2 tx without authorizationList', () => {
+      expect(() => validateAuthorizationList(2, undefined)).not.to.throw();
+    });
+
+    it('should not throw for type 1 tx without authorizationList', () => {
+      expect(() => validateAuthorizationList(1, undefined)).not.to.throw();
+    });
+
+    it('should not throw for type 0 tx without authorizationList', () => {
+      expect(() => validateAuthorizationList(0, undefined)).not.to.throw();
+    });
+
+    it('should throw for type 2 tx with authorizationList', () => {
+      expect(() => validateAuthorizationList(2, [authEntry])).to.throw('not supported for non-type-4 transactions');
+    });
+
+    it('should throw for type 1 tx with authorizationList', () => {
+      expect(() => validateAuthorizationList(1, [authEntry])).to.throw('not supported for non-type-4 transactions');
+    });
+
+    it('should throw for type 0 tx with authorizationList', () => {
+      expect(() => validateAuthorizationList(0, [authEntry])).to.throw('not supported for non-type-4 transactions');
+    });
+
+    it('should throw for type 2 tx with empty authorizationList array', () => {
+      expect(() => validateAuthorizationList(2, [])).to.throw('not supported for non-type-4 transactions');
+    });
+  });
+
+  describe('type 4 transactions', () => {
+    it('should not throw for type 4 tx with non-empty authorizationList', () => {
+      expect(() => validateAuthorizationList(4, [authEntry])).not.to.throw();
+    });
+
+    it('should throw for type 4 tx without authorizationList', () => {
+      expect(() => validateAuthorizationList(4, undefined)).to.throw('must be set for type 4 transactions');
+    });
+
+    it('should throw for type 4 tx with null authorizationList', () => {
+      expect(() => validateAuthorizationList(4, null)).to.throw('must be set for type 4 transactions');
+    });
+
+    it('should throw for type 4 tx with empty authorizationList', () => {
+      expect(() => validateAuthorizationList(4, [])).to.throw('cannot be empty');
+    });
+  });
+});
